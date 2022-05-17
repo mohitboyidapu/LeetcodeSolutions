49865cb957de4bb0f24bbe1dcda6e78873170792
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-05-17 11:49:28 +0530

    Time: 815 ms (29.75%), Space: 164 MB (22.94%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -9,39 +9,26 @@
  */
 
 class Solution {
+private:
+    
+    void inorder(TreeNode* root, TreeNode* cloned, TreeNode* target, TreeNode* &ans){
+        if(root == NULL ){
+            return;
+        }
+        
+        if(root->val == target->val) {
+            ans = cloned;
+            
+            return;
+        }
+        inorder(root->left, cloned->left, target, ans);
+        inorder(root->right, cloned->right, target, ans);
+
+    }
 public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        
-        stack<TreeNode*> s;
-        stack<TreeNode*> sdup;
-
-        TreeNode* curr = original;
-        TreeNode* dup = cloned;
-
-
-        while(true){
-            if(curr != NULL){
-                s.push(curr);
-                sdup.push(dup);
-
-                curr = curr->left;
-                dup = dup->left;
-            }
-            else{
-                if(s.empty()) break;
-                
-                curr = s.top();
-                dup = sdup.top();
-                s.pop();
-                sdup.pop();
-
-                if(curr == target) return dup;
-
-                curr = curr->right;
-                dup = dup->right;
-
-            }
-        }
-        return NULL;
+        TreeNode* ans = NULL;
+        inorder(original, cloned, target, ans);
+        return ans;
     }
 };
